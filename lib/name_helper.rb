094c5ac551020bcc6b016e59df2190163a686ff4
Karina Tiurina <rktiurina@gmail.com>
2018-01-31 12:26:48 +0300

    add methods

--- lib/name_helper.rb
+++ lib/name_helper.rb
@@ -2,6 +2,16 @@ require "name_helper/version"
 
 module NameHelper
   def self.tableize(controller_class_name)
-    controller_class_name.to_s.gsub("sController", '').tableize
+    edit_class_name(controller_class_name).tableize
+  end
+
+  def self.underscore(controller_class_name)
+    edit_class_name(controller_class_name).underscore
+  end
+
+  private
+
+  def self.edit_class_name(controller_class_name)
+    controller_class_name.to_s.gsub("sController", '')
   end
 end
