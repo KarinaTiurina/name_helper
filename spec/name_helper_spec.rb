867cd60fc2b382477b0c932d88a052aa8263d6f7
Karina Tiurina <rktiurina@gmail.com>
2018-01-31 12:46:24 +0300

    rspec: tableize

--- spec/name_helper_spec.rb
+++ spec/name_helper_spec.rb
@@ -3,8 +3,8 @@ RSpec.describe NameHelper do
     expect(NameHelper::VERSION).not_to be nil
   end
 
-  # it "convert controller name to table name" do
-  #   controller_class_name = 'ItemFilesController'
-  #   expect(NameHelper.tableize(controller_class_name)).to eq("item_files")
-  # end
+  it "convert controller name to table name" do
+    controller_class_name = 'ItemFilesController'
+    expect(NameHelper.tableize(controller_class_name)).to eq("item_files")
+  end
 end
